1c704faa0c6ca9b609aa8a97653a3ffe62c6077d
Angela Mumba <malaika.mumba@gmail.com>
2019-06-01 18:25:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,14 @@
 class Person
   #your code here
+  
+  attr_accessor :name, :age, :location, :user_name
+ 
+  def initialize(user_name:, name:, age:, location:)
+    @user_name = user_name
+    @name = name
+    @location = location
+    @age = age
+  end
   :name, :birthday, :hair_color, :eye_color, :height, 
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
