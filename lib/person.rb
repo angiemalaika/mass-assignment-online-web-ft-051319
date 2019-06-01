04d5f8a42492ac8a62574d296dfb314a2813b32b
Angela Mumba <malaika.mumba@gmail.com>
2019-06-01 18:26:49 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,15 @@
 class Person
   #your code here
   
-  attr_accessor :name, :age, :location, :user_name
+  attr_accessor :name,:birthday,:hair_color,:eye_color,:height,:weight,:handed,:complexion, :t_shirt_size,:wrist_size,:glove_size,:pant_length,:pant_width
  
-  def initialize(user_name:, name:, age:, location:)
-    @user_name = user_name
-    @name = name
-    @location = location
-    @age = age
-  end
-  :name, :birthday, :hair_color, :eye_color, :height, 
+  def initialize(:name, :birthday, :hair_color, :eye_color, :height, 
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
+)
+   :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
+  end
+
 end
