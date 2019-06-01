edb51b6adf2e65885014b39375451c6c23220689
Angela Mumba <malaika.mumba@gmail.com>
2019-06-01 18:35:10 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -19,11 +19,11 @@ class Person
     @pant_width = pant_width
   end
 
-class User
-  attr_accessor :name, :user_name, :age, :location, :bio
+# class User
+#   attr_accessor :name,:birthday,:hair_color,:eye_color,:height,:weight,:handed,:complexion, :t_shirt_size,:wrist_size,:glove_size,:pant_length,:pant_width
  
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
-  end
-end
+#   def initialize(attributes)
+#     attributes.each {|key, value| self.send(("#{key}="), value)}
+#   end
+# end
 end
